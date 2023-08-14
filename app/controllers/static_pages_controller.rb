18564e6c59822cd29a9f76390be2613a84d16ba0
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-14 11:59:26 -0700

    Add search function with user ID and a response

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
-    flickr = Flicker.new
+    @flickr = Flickr.new
+    p params[:user_id] unless params[:user_id].nil?
+    @flickr_response = @flickr.photos.search(user_id: params[:user_id]) unless params[:user_id].nil?
   end
 end
